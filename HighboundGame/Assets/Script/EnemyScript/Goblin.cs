7922d116b356c385b1c2c8c5451e6150cd4c263f
Rubin-B <rubinbayfield2@gmail.com>
2022-04-25 18:49:54 +0100

    Added Unity Project of Highbound Game

---
+++ HighboundGame/Assets/Script/EnemyScript/Goblin.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+
+    private float playerDistance = 0;
+    private Transform player;
+    private float maxDis = 30;
+    private float minDis = 5;
+
+    private float moveSpeed = 5f;
+    private float attackMoveSpeed = 6f;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        player = GameObject.Find("Player").transform;
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (player == null)
+            return;
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Sword And Shield Idle") && anim.GetBool("isAttacking") == true)
+        {
+            anim.SetBool("isAttacking", false);
+        }
+
+        playerDistance = Vector3.Distance(gameObject.transform.position, player.position);
+
+        if(playerDistance <= maxDis && playerDistance > minDis && anim.GetBool("isAttacking") == false)
+        {
+            anim.SetBool("isMoving", true);
+            rotateToPlayer();
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }else if(playerDistance > maxDis || playerDistance <= minDis)
+        {
+            anim.SetBool("isMoving", false);
+            if (playerDistance <= minDis)
+            {
+                if (anim.GetBool("isAttacking") == false)
+                {
+                    anim.SetBool("isAttacking", true);
+                    rotateToPlayer();
+                }
+                else
+                {
+                    transform.position += transform.forward * attackMoveSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private void rotateToPlayer()
+    {
+        var lookPos = player.position - transform.position;
+        lookPos.y = 0.0f;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = rotation;
+    }
+}
---
+++ HighboundGame/Assets/Script/ObstacleScript/MovingPlatform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Transform posA;
+    private Transform posB;
+
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private bool dirA = false;
+
+    private void Start()
+    {
+        posA = gameObject.transform.parent.GetChild(0);
+        posB = gameObject.transform.parent.GetChild(1);
+    }
+
+    private void Update()
+    {
+        if (dirA == false)
+        {
+            gameObject.transform.position += transform.right * speed * Time.deltaTime;    
+        }else
+        {
+            gameObject.transform.position += -transform.right * speed * Time.deltaTime;
+        }
+
+        if(gameObject.transform.position.x >= posB.position.x)
+        {
+            dirA = true;
+        }else if(gameObject.transform.position.x <= posA.position.x)
+        {
+            dirA = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.name == "Player")
+            c.gameObject.transform.SetParent(gameObject.transform);
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.name == "Player")
+            c.gameObject.transform.parent = null;
+    }
+
+}
