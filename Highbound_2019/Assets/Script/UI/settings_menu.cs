e4fcded4c170aaf1540dd694b56e7287ad1ed1f5
Odin Branigan <odinbranigan@gmail.com>
2022-04-29 19:21:31 +0100

    Fully functional title screen and settings menu
    A title screen containing an interactive and functional main menu and settings menu with audio

---
+++ Highbound_2019/Assets/Script/UI/settings_menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class settings_menu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+
+    public void SetVolume(float gameVolume)
+    {
+        mainMixer.SetFloat("gameVolume", gameVolume);
+    }
+
+    public void ToggleFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
