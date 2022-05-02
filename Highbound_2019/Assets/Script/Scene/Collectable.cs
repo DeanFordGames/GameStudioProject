3fddcaafc837e24d54b12fb134b0e324ed12208f
DeanFordGames <jdmadman56@gmail.com>
2022-05-02 21:57:01 +0100

    Added Collectable

---
+++ Highbound_2019/Assets/Script/Scene/Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private int points = 10;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.name == "Player")
+        {
+            ScoreSystem score = GameObject.Find("Score").GetComponent<ScoreSystem>();
+            score.changeScore(points);
+            Destroy(gameObject);
+        }
+    }
+}
