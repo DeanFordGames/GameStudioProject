b3d664de93619ef1330454503f58fec03e1adc5d
DeanFordGames <jdmadman56@gmail.com>
2022-05-02 23:15:23 +0100

    Background music now plays in every scene

---
+++ Highbound_2019/Assets/Script/UI/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private BackgroundMusic instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
